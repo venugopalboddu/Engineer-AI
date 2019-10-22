67d4355f3434a85f5aac3309c4ea10556c441e43
Venu Gopal Boddu <vb19419@corp.ojas-it.com>
2019-10-22 14:53:31 +0530

    Updated_Task

---
+++ src/app/grd-filter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { GrdFilterPipe } from './grd-filter.pipe';
+
+describe('GrdFilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new GrdFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
